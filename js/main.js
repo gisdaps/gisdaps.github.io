23a55886aaff255d9c87778e320ed53adc08c0c7
Daffa Audya <daffaaudyapramana03@gmail.com>
2023-11-17 14:07:58 +0700

    update

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ import { get } from "https://jscroot.github.io/api/croot.js";
 import {getCookie} from "https://jscroot.github.io/cookie/croot.js";
 import { URLGeoJson } from "./template/template.js";
 import { MakeGeojsonFromAPI, responseData, AddLayerToMAP, drawer } from "./controller/controller.js";
-import {map} from './config/config.js';
+import {map} from './config/configpeta.js';
 import {onClosePopupClick,onDeleteMarkerClick,onSubmitMarkerClick,onMapClick,onMapPointerMove,disposePopover, GetLonLat} from './controller/popup.js';
 import {onClick} from 'https://jscroot.github.io/element/croot.js';
 import {getAllCoordinates} from './controller/cog.js';
